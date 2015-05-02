4d33062a8d03219b31561b06bfca42199b3b0d20
timakin <timaki.st@gmail.com>
2015-05-03 04:08:20 +0900

    Devide ts hack and ts pop

--- commands.go
+++ commands.go
@@ -27,7 +27,7 @@ var commandAll = cli.Command{
 //	Action: doBiz,
 //}
 var commandHack = cli.Command{
-	Name:  "test",
+	Name:  "hack",
 	Usage: "",
 	Description: `
 `,
@@ -67,6 +67,12 @@ func doAll(c *cli.Context) {
 //func doBiz(c *cli.Context) {
 //}
 func doHack(c *cli.Context) {
+	re := make(chan loader.ResultData)
+	go loader.GetRedditFeed(re)
+	reres := <- re
+	var REData loader.Feed = &reres
+	REData.Display()
+	displayRSSFeed("[HackerNews]", "https://news.ycombinator.com/rss")
 	displayRSSFeed("[EchoJS]", "http://www.echojs.com/rss")
 	displayRSSFeed("[RubyDaily]", "http://feeds.rubydaily.org/RubyDaily")
 	displayRSSFeed("[Hatena]", "http://b.hatena.ne.jp/search/tag?q=%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0&users=10&mode=rss")
