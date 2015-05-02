4742cd0d9e89d2650ece283c7b41f256dad5d556
timakin <timaki.st@gmail.com>
2015-05-02 19:12:45 +0900

    Fix Reddit getter

--- commands.go
+++ commands.go
@@ -36,17 +36,21 @@ var commandHack = cli.Command{
 }
 
 func doAll(c *cli.Context) {
-	loader.GetRedditFeed()
 	hn := make(chan loader.ResultData)
 	ph := make(chan loader.ResultData)
+	re := make(chan loader.ResultData)
 	go loader.GetHNFeed(hn)
 	go loader.GetPHFeed(ph)
+	go loader.GetRedditFeed(re)
 	hnres := <- hn
 	phres := <- ph
+	reres := <- re
 	var HNData loader.Feed = &hnres
 	var PHData loader.Feed = &phres
+	var REData loader.Feed = &reres
 	HNData.Display()
 	PHData.Display()
+	REData.Display()
 }
 
 func doBiz(c *cli.Context) {
